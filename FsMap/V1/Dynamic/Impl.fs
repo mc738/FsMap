17db02e0f43f174635f509484d50d30cb8a832bd
Max <me@mclifford.dev>
2025-05-23 22:40:42 +0100

    chore(dynamic) Work on dynamic matter

--- FsMap/V1/Dynamic/Impl.fs
+++ FsMap/V1/Dynamic/Impl.fs
@@ -1,16 +1,27 @@
 ﻿namespace FsMap.V1.Dynamic
 
+open System
+open System.Reflection
+
 [<AutoOpen>]
 module Impl =
-    
-    
-    type DynamicMapper() =
-        
-        member _.Map<'TFrom, 'TTo>(fromType: 'TFrom) =
-            
-            
-            
-            ()
-    
-    ()
 
+    type MappedType =
+        { Type: Type
+          DefaultProperty: PropertyInfo }
+
+    and MappedProperty =
+        { Name: string
+          PropertyInfo: PropertyInfo }
+
+
+    type DynamicMapper() =
+
+        member _.Map<'TFrom, 'TTo>(fromType: 'TFrom) =
+
+
+
+            ()
+       
+
+    ()
