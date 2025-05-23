9820b87a75f049bfe97d4d86c79fd919d572e346
Max <me@mclifford.dev>
2025-05-23 22:39:34 +0100

    chore(json): Added Schema.fs

---
+++ FsMap/V1/Generated/Json/Schema.fs
@@ -0,0 +1,12 @@
+﻿namespace FsMap.V1.Generated.Json
+
+open Microsoft.Json.Schema
+
+module Schema =
+    
+    
+    let readSchema (str: string) =
+        
+        SchemaReader.ReadSchema(str, "")
+
+    
