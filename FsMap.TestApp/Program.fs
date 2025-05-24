e0a3509199f7c72be02990d189eb5a78bfcf7f50
Max <me@mclifford.dev>
2025-05-25 00:55:26 +0100

    feat(app): Work on Program.fs

--- FsMap.TestApp/Program.fs
+++ FsMap.TestApp/Program.fs
@@ -3,9 +3,10 @@ open System.Reflection
 open FsMap.V1.Core.Attributes
 open FsMap.V1.Core.Models
 open FsMap.V1.Generated.Json
+open FsMap.V1.Generated.Json.Schema
 
 module GenTest =
-    
+
     open FsMap.V1.Generated.CodeGeneration.Common
 
     let members =
@@ -40,7 +41,7 @@ module GenTest =
 
         generated "MyModule" members
         |> fun r -> File.WriteAllText("C:\\Users\\mclif\\Projects\\dotnet\\FsMap\\FsMap.TestApp\\Test.fs", r)
-        
+
 
 type Foo =
     { Value: string }
@@ -57,14 +58,22 @@ type Bar =
     static member Default = { Name = ""; Value = 42 }
 
 module JsonSchemaTest =
-    
-    let example1Path = "C:\\Users\\mclif\\Projects\\data\\json_schema\\examples\\example1.json"
-    
-    
+
+    open FsMap.V1.Generated.CodeGeneration.Common
+
+    let example1Path =
+        "C:\\Users\\mclif\\Projects\\data\\json_schema\\examples\\example1.json"
+
+
     let run _ =
         let schema = Schema.readSchemaFile example1Path
-        
-        
+
+        buildModels schema
+        |> Result.map ModuleMember.Record
+        |> Result.iter (fun mm ->
+            generated "Examples" [ mm ]
+            |> fun r -> File.WriteAllText("C:\\Users\\mclif\\Projects\\dotnet\\FsMap\\FsMap.TestApp\\Examples.fs", r))
+
         ()
 
 module Test =
@@ -85,7 +94,7 @@ module Test =
 
         let j =
             Schema.readSchemaFile "C:\\Users\\mclif\\Projects\\data\\json_schema\\examples\\example1.json"
-            
+
         ()
 
 GenTest.run ()
