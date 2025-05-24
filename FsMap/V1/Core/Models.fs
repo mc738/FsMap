95a2567b410ea3aff23d2ea80fcb556ef41ea64b
Max <me@mclifford.dev>
2025-05-24 12:35:05 +0100

    feat(core): Added Models.fs

---
+++ FsMap/V1/Core/Models.fs
@@ -0,0 +1,98 @@
+﻿namespace FsMap.V1.Core
+
+open System
+open System.Text.Json
+open System.Text.Json.Serialization
+
+module Models =
+
+    [<RequireQualifiedAccess>]
+    type BaseType =
+        | Boolean
+        // Bytes
+        | Byte
+        | SByte
+        // Int
+        | Int16
+        | Int32
+        | Int64
+        | UInt16
+        | UInt32
+        | UInt64
+        // Float
+        | Single
+        | Double
+        // Decimal
+        | Decimal
+        // Date/times
+        | DateTime
+        | DateTimeOffset
+        | TimeSpan
+        // Strings
+        | String
+        // Other
+        | Guid
+
+        member bt.GetCLRType() =
+            match bt with
+            | Boolean -> typeof<bool>
+            | Byte -> typeof<byte>
+            | SByte -> typeof<sbyte>
+            | Int16 -> typeof<int16>
+            | Int32 -> typeof<int>
+            | Int64 -> typeof<int64>
+            | UInt16 -> typeof<uint16>
+            | UInt32 -> typeof<uint>
+            | UInt64 -> typeof<uint64>
+            | Single -> typeof<single>
+            | Double -> typeof<double>
+            | Decimal -> typeof<decimal>
+            | DateTime -> typeof<DateTime>
+            | DateTimeOffset -> typeof<DateTimeOffset>
+            | TimeSpan -> typeof<TimeSpan>
+            | String -> typeof<string>
+            | Guid -> typeof<Guid>
+
+    type [<RequireQualifiedAccess>] FieldType =
+        | BaseType of BaseType
+        | Record of RecordDefinition
+        | DiscriminatedUnion of DiscriminatedUnionDefinition
+        | Function of FunctionDefinition
+        | Reference of Name: string
+
+    and RecordDefinition =
+        { Namespace: string option
+          Name: string
+          Attributes: AttributeDefinition list
+          Fields: FieldDefinition list }
+
+    and FieldDefinition =
+        { Name: string
+          FieldType: FieldType
+          Attributes: AttributeDefinition list
+          Optional: bool }
+
+    and DiscriminatedUnionDefinition =
+        { Namespace: string option
+          Name: string
+          Attributes: Attribute list }
+
+    and DiscriminatedUnionCaseDefinition =
+        { Name: string
+          Attributes: Attribute list }
+
+    and FunctionDefinition = { Name: string }
+
+    and AttributeDefinition =
+        { Namespace: string option
+          Name: string }
+
+    and ModuleDefinition =
+        { Name: string option
+          Attributes: Attribute list }
+
+    and [<RequireQualifiedAccess>] ModuleMember =
+        | Record of RecordDefinition
+        | DiscriminatedUnion of DiscriminatedUnionDefinition
+        | Function of FunctionDefinition
+        
