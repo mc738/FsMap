fe3b1c56430405216a25a4c5a794e2f3e049b5f7
Max <me@mclifford.dev>
2025-05-25 00:50:17 +0100

    feat(schema): Work on Schema.fs

--- FsMap/V1/Generated/Json/Schema.fs
+++ FsMap/V1/Generated/Json/Schema.fs
@@ -3,10 +3,14 @@
 //#nowarn "1010103001"
 
 open System.IO
+open System.Reflection.Metadata
+open FsMap.V1.Core
+open FsMap.V1.Core.Models
 open Microsoft.Json.Schema
+open FsToolbox.Extensions.Strings
 
 module Schema =
-    
+
     [<RequireQualifiedAccess>]
     type JsonType =
         | Array
@@ -17,7 +21,7 @@ module Schema =
         | Null
         | Object
         | String
-        
+
         static member Get(schemaValue: SchemaType) =
             match schemaValue with
             | SchemaType.Array -> JsonType.Array
@@ -29,7 +33,7 @@ module Schema =
             | SchemaType.Object -> JsonType.Object
             | SchemaType.String -> JsonType.String
             | _ -> JsonType.None
-            
+
         member jt.IsScalar() =
             match jt with
             | Array -> false
@@ -40,8 +44,9 @@ module Schema =
             | Null -> true
             | Object -> false
             | String -> true
-            
-        [<CompilerMessage("This method will throw for non-scalar types, it is assumed types will be checked with JsonType.IsScalar() first or errors. To suppress this error add #nowarn \"1010103001\"", 1010103001)>]
+
+        [<CompilerMessage("This method will throw for non-scalar types, it is assumed types will be checked with JsonType.IsScalar() first or errors. To suppress this error add #nowarn \"1010103001\"",
+                          1010103001)>]
         member jt.GetDotNetScalarType() =
             match jt with
             | Array -> invalidOp "JsonType.Array is not a scalar type"
@@ -52,9 +57,9 @@ module Schema =
             | Null -> failwith "" // typeof<null>
             | Object -> invalidOp "JsonType.Object is not a scalar type"
             | String -> Some typeof<string>
-    
+
         member jt.ToFieldDefinition() = ()
-    
+
     [<RequireQualifiedAccess>]
     type BuiltInFormat =
         | DateTime
@@ -76,7 +81,7 @@ module Schema =
         | JsonPointer
         | RelativeJsonPointer
         | Regex
-        
+
         static member TryDeserialize(value: string) =
             match value with
             | "date-time" -> Some BuiltInFormat.DateTime
@@ -99,55 +104,121 @@ module Schema =
             | "relative-json-pointer" -> Some BuiltInFormat.RelativeJsonPointer
             | "regex" -> Some BuiltInFormat.Regex
             | _ -> None
-    
-    let ``get built-in format`` (str: string)=
-        ()
-    
-    let build (schema: JsonSchema) =
-        
-        schema.Title
-        
+
+    let ``get built-in format`` (str: string) = ()
+
+    let rec build (schema: JsonSchema) =
+
+        let fields =
+            schema.Properties
+            |> Seq.map (fun kv ->
+                match kv.Value.Type.Count with
+                | 1 ->
+                    match kv.Value.Type[0] with
+                    | SchemaType.Array -> failwith "todo"
+                    | SchemaType.Boolean ->
+                        ({ Name = kv.Key.ToPascalCase()
+                           FieldType = FieldType.BaseType BaseType.Boolean
+                           Attributes = []
+                           Optional = false }
+                        : Models.FieldDefinition)
+                    | SchemaType.None -> failwith "todo"
+                    | SchemaType.Integer ->
+                        ({ Name = kv.Key.ToPascalCase()
+                           FieldType = FieldType.BaseType BaseType.Int32
+                           Attributes = []
+                           Optional = false }
+                        : Models.FieldDefinition)
+                    | SchemaType.Number ->
+                        ({ Name = kv.Key.ToPascalCase()
+                           FieldType = FieldType.BaseType BaseType.Double
+                           Attributes = []
+                           Optional = false }
+                        : Models.FieldDefinition)
+                    | SchemaType.Null -> failwith "todo"
+                    | SchemaType.Object -> failwith "todo"
+                    | SchemaType.String ->
+                        //match kv.Value.Format
+
+                        ({ Name = kv.Key.ToPascalCase()
+                           FieldType = FieldType.BaseType BaseType.String
+                           Attributes = []
+                           Optional = false }
+                        : Models.FieldDefinition)
+                    | _ -> failwith "todo"
+                | 0 ->
+                    // No type defined...
+                    failwith "TODO  - handle"
+                | _ ->
+                    // If there is more than one valid type,
+                    // then any of these types if a valid value.
+                    // So this could be mapped as a DU.
+                    //
+                    // If there are 2 value types and one is a null
+                    // this can be simply put as an option
+
+
+                    failwith "TODO - handle")
+            |> List.ofSeq
+
+        ({ Namespace = None
+           Name = schema.Title.ToPascalCase()
+           Attributes = []
+           Fields = fields }
+        : RecordDefinition)
+
+
+    //schema.Title
+
+    //match schema.Type.Count with
+    //| 1 ->
+    //    match schema.Type[0] with
+    //    | SchemaType.Array -> failwith "todo"
+    //    | SchemaType.Boolean ->
+    //        ({ Name = schema.Title
+    //           FieldType = FieldType.BaseType BaseType.Boolean
+    //           Attributes = []
+    //           Optional = false }
+    //        : Models.FieldDefinition)
+    //    | SchemaType.None -> failwith "todo"
+    //    | SchemaType.Integer -> failwith "todo"
+    //    | SchemaType.Number -> failwith "todo"
+    //    | SchemaType.Null -> failwith "todo"
+    //    | SchemaType.Object -> failwith "todo"
+    //    | SchemaType.String -> failwith "todo"
+    //    | _ -> failwith "todo"
+    //| 0 ->
+    //    // No type defined...
+    //    failwith "TODO  - handle"
+    //| _ ->
+    //    // If there is more than one valid type,
+    //    // then any of these types if a valid value.
+    //    // So this could be mapped as a DU.
+    //    //
+    //    // If there are 2 value types and one is a null
+    //    // this can be simply put as an option
+    //
+
+    //failwith "TODO - handle"
+
+    let buildModels (schema: JsonSchema) =
+
         match schema.Type.Count with
+        | 0 -> Result.Error "No root type defined"
         | 1 ->
-            schema.
-            
-            
             match schema.Type[0] with
-            | SchemaType.Array -> ()
-            | SchemaType.Boolean -> ()
-            | SchemaType.None -> failwith "todo"
-            | SchemaType.Integer -> failwith "todo"
-            | SchemaType.Number -> failwith "todo"
-            | SchemaType.Null -> failwith "todo"
-            | SchemaType.Object -> failwith "todo"
-            | SchemaType.String -> failwith "todo"
-            ()
-        | 0 ->
-            // No type defined...
-            failwith "TODO  - handle"
-        | _ ->
-            // If there is more than one valid type,
-            // then any of these types if a valid value.
-            // So this could be mapped as a DU.
-            //
-            // If there are 2 value types and one is a null
-            // this can be simply put as an option 
-            
-            
-            failwith "TODO - handle"
-        
-        
-        ()
-    
-    
+            | SchemaType.Object -> build schema |> Ok // Ok()
+            | _ -> Result.Error "Root type is not object"
+        | _ -> Result.Error "Multiple types in root object not supported"
+
     let readSchema (str: string) =
-        
+
         SchemaReader.ReadSchema(str, "")
 
-    
+
     let readSchemaFile (path: string) =
         use fs = File.OpenRead path
-        
+
         use sr = new StreamReader(fs)
-        
+
         SchemaReader.ReadSchema(sr, "")
