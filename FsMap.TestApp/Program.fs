dece9ba6ede4a2c088adb232c28334d4a6b6d3ce
Max <me@mclifford.dev>
2025-05-25 22:27:36 +0100

    feat(app): Work on app

--- FsMap.TestApp/Program.fs
+++ FsMap.TestApp/Program.fs
@@ -56,26 +56,41 @@ type Bar =
     [<Default>]
     static member Default = { Name = ""; Value = 42 }
 
+module JsonSchemaTest =
+    
+    let example1Path = "C:\\Users\\mclif\\Projects\\data\\json_schema\\examples\\example1.json"
+    
+    
+    let run _ =
+        let schema = Schema.readSchemaFile example1Path
+        
+        
+        ()
 
 module Test =
 
-    let t = typeof<Foo>
+    let run () =
+        let t = typeof<Foo>
 
-    let props = t.GetProperties()
+        let props = t.GetProperties()
 
-    let ``has default attribute`` (pi: PropertyInfo) =
-        pi.GetCustomAttribute<DefaultAttribute>() |> Option.ofObj |> Option.isSome
+        let ``has default attribute`` (pi: PropertyInfo) =
+            pi.GetCustomAttribute<DefaultAttribute>() |> Option.ofObj |> Option.isSome
 
-    let defaultProperty =
-        t.GetProperties(BindingFlags.Static ||| BindingFlags.Public)
-        |> Array.tryFind ``has default attribute``
+        let defaultProperty =
+            t.GetProperties(BindingFlags.Static ||| BindingFlags.Public)
+            |> Array.tryFind ``has default attribute``
 
-    let dv = defaultProperty |> Option.map (fun dp -> dp.GetValue(null))
+        let dv = defaultProperty |> Option.map (fun dp -> dp.GetValue(null))
 
-    let j =
-        Schema.readSchemaFile "C:\\Users\\mclif\\Projects\\data\\json_schema\\examples\\example1.json"
+        let j =
+            Schema.readSchemaFile "C:\\Users\\mclif\\Projects\\data\\json_schema\\examples\\example1.json"
+            
+        ()
 
 GenTest.run ()
+
+JsonSchemaTest.run ()
 
 // For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
